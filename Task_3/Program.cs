a69b7d903582c5371a094c0acb82069d7b739b60
svetlana <masterica75@mail.ru>
2022-12-14 17:52:06 +0700

    Added Task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int n = 8;
+int[] array = new int[n];
+Random rand = new Random();
+int[] GetNewArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, n);
+
+        Console.Write(array[i] + ", ");
+
+    }
+    return array;
+}
+GetNewArr(array);
